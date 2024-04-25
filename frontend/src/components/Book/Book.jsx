7c450f3acd797cfc427844295997cd6cd6718aac
JuanAntonio1234 <juanantonioguerreroorihuela@gmail.com>
2024-04-25 10:15:26 +0100

    Home done

--- frontend/src/components/Book/Book.jsx
+++ frontend/src/components/Book/Book.jsx
@@ -8,17 +8,13 @@ const Book = (props) => {
     
     useEffect(() => {
         setBook(props.book)
-        console.log(props.book && props.book.image)
     }, [props.book])
 
 
   return (
     <div className="book">
       {book ? <>
-        <h3>{book.name}</h3>
-        <img className="book-image" src={"../../" + book.image} alt={book.name} />
-        <p>{book.description}</p>
-      </> : "Loading..."}
+        <img className="book-image" src={book.image} alt=""/>      </> : "Loading..."}
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -27,11 +27,34 @@ const Home= () => {
 
       <h1 id="h1title">HOME</h1>
 
-      <div>
-        <h1>Libros más populares</h1>
-        <div>
+      <div class="row-display">
+        <h1 id="popular-books-title">Libros más populares</h1>
+        <div id="book-display">
           {libros ? <Book book={libros[0]}/> : "Loading..."}
-          
+          {libros ? <Book book={libros[1]}/> : "Loading..."}
+          {libros ? <Book book={libros[7]}/> : "Loading..."}
+          {libros ? <Book book={libros[3]}/> : "Loading..."}
+          {libros ? <Book book={libros[4]}/> : "Loading..."}
+          {libros ? <Book book={libros[5]}/> : "Loading..."}
+          {libros ? <Book book={libros[11]}/> : "Loading..."}
+          {libros ? <Book book={libros[38]}/> : "Loading..."}
+          {libros ? <Book book={libros[27]}/> : "Loading..."}
+          {libros ? <Book book={libros[23]}/> : "Loading..."}
+        </div>
+      </div>
+
+      <div class="row-display">
+        <h1 id="popular-books-title">Libros más vendidos</h1>
+        <div id="book-display">
+          {libros ? <Book book={libros[20]}/> : "Loading..."}
+          {libros ? <Book book={libros[31]}/> : "Loading..."}
+          {libros ? <Book book={libros[17]}/> : "Loading..."}
+          {libros ? <Book book={libros[33]}/> : "Loading..."}
+          {libros ? <Book book={libros[24]}/> : "Loading..."}
+          {libros ? <Book book={libros[15]}/> : "Loading..."}
+          {libros ? <Book book={libros[8]}/> : "Loading..."}
+          {libros ? <Book book={libros[32]}/> : "Loading..."}
+
         </div>
       </div>
 
