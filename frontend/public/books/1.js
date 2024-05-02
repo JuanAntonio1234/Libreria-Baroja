77a050ff40431fcb4e15e87b3bc40b81dd33f3d5
JuanAntonio1234 <juanantonioguerreroorihuela@gmail.com>
2024-05-02 09:53:35 +0100

    added title in /read-Book/:id

--- frontend/public/books/1.js
+++ frontend/public/books/1.js
@@ -1,3 +1,6 @@
-const book = "En un agujero en el suelo, vivía un hobbit. No un agujero húmedo, sucio, repugnante, con restos de gusanos y olor a fango, ni tampoco un agujero seco, desnudo y arenoso, sin nada en que sentarse o que comer: era un agujero-hobbit, y eso significa comodidad \nEra un hobbit, y eso significa que era muy respetable. No era del todo solitario, porque tenía varios parientes más o menos distantes en las colinas cercanas. Los hobbits, en general, tienen un aspecto agradable y vivaz, y este lo era mucho más. Sus puertas estaban pintadas de verde, y en las alfombras del vestíbulo había flores talladas; en todas las estanterías había mucha madera de fresno, y tenía muchas ventanas redondas, además de muchas puertas rectangulares."
+const book = {
+    title: "El señor de los anillos",
+    book: "En un agujero en el suelo, vivía un hobbit. No un agujero húmedo, sucio, repugnante, con restos de gusanos y olor a fango, ni tampoco un agujero seco, desnudo y arenoso, sin nada en que sentarse o que comer: era un agujero-hobbit, y eso significa comodidad \nEra un hobbit, y eso significa que era muy respetable. No era del todo solitario, porque tenía varios parientes más o menos distantes en las colinas cercanas. Los hobbits, en general, tienen un aspecto agradable y vivaz, y este lo era mucho más. Sus puertas estaban pintadas de verde, y en las alfombras del vestíbulo había flores talladas; en todas las estanterías había mucha madera de fresno, y tenía muchas ventanas redondas, además de muchas puertas rectangulares."
+}
 
 export default book
--- frontend/src/pages/ReadBook/ReadBook.jsx
+++ frontend/src/pages/ReadBook/ReadBook.jsx
@@ -4,31 +4,34 @@ import { useEffect, useState } from "react";
 import { validateUser } from "../../services/userService";
 
 const ReadBook = () => {
-  const [book, setBook] = useState({});
+  const [book, setBook] = useState("");
+  const [bookTitle , setBookTitle] = useState("");
   const { id } = useParams();
   const token = localStorage.getItem("token");
 
   useEffect(() => {
     const getUser = async () => {
       const user = await validateUser(token);
-      console.log(user.data.rol);
       if (user.data.rol === "user") {
         alert("Debes suscribirte para acceder a esta sección.");
         window.location.href = "/home";
       } else {
         import(`/public/books/${id}.js`).then((module) => {
-          setBook(module);
+          setBook(module.default.book);
+          setBookTitle(module.default.title);
+          console.log(book);
+          console.log(bookTitle);
         });
       }
     };
 
     getUser();
-  }, []);
+  }, [book]);
 
   return (
     <div id="read-book">
-      <h1>Soy el libro con ID {id}</h1>
-      <p>{book ? book.default : "Loading..."}</p>
+      <h1>{bookTitle ? bookTitle : "Loading..."}</h1>
+      <p id="book"> {book ? book : "Loading..."} </p>
     </div>
   );
 };
